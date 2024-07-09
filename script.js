5c3183120da23a36213e650574a728f0dd10210d
Navya <navya.bhatt31@gmail.com>
2024-07-09 18:11:22 +0530

    changes

--- script.js
+++ script.js
@@ -10,4 +10,14 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
+    document.querySelectorAll('.project-card').forEach(card => {
+        card.addEventListener('mousemove', (e) => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+    
+            card.style.setProperty('--mouse-x', `${x}px`);
+            card.style.setProperty('--mouse-y', `${y}px`);
+        });
+    });
 })();
